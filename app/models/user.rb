c4868d0252dd852a61f046797f1fa0861ef714a2
dannyfern <danielanthonyharb@gmail.com>
2020-06-25 15:19:04 +1000

    finished test app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_many :microposts
+    validates_presence_of :name, :email
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
-  get 'about', to: 'posts#about'
+  root 'users#index'
+  
 end
